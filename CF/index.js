91aac22364211f1b6e952b3445b8e92cd2acf195
Frank Schmid <fschmid@stashimi.com>
2016-02-26 18:07:54 +0100

    Last changes - now working

--- CF/index.js
+++ CF/index.js
@@ -3,12 +3,10 @@
  * Retrieve CloudFormation information within a lambda.
  */
 
-var async = require('asyncawait/async'),
-    await = require('asyncawait/await'),
-    Promise = require('bluebird'),
+var Promise = require('bluebird'),
     AWS = require('aws-sdk');
 
-var cloudformation = new AWS.CloudFormation({apiVersion: '2010-05-15'});
+var cloudformation = new AWS.CloudFormation({apiVersion: '2010-05-15', region: process.env.SERVERLESS_REGION });
 
 var CF = {
 };
@@ -16,43 +14,48 @@ var CF = {
 /**
  * Load CF output variables into environment
  */
-CF.loadVars = async (function() {
+CF.loadVars = function () {
   // Build CF stack name
-  if (!process.env.SERVERLESS_PROJECT || !process.env.SERVERLESS_STAGE) {
-    throw new Error("Serverless environment not set");
+  if (!process.env.SERVERLESS_PROJECT || !process.env.SERVERLESS_STAGE || !process.env.SERVERLESS_REGION) {
+    return Promise.fail(new Error("Serverless environment not set"));
   }
-  stackName = process.env.SERVERLESS_PROJECT + "-" + process.env.SERVERLESS_STAGE + "-r";
 
-  return await (CF.describeCFStack(stackName)
-                  .then(function(stackDescription) {
-                    if (!stackDescription.hasOwnProperty('Outputs') || stackDescription.Outputs.constructor !== Array) {
-                      return Promise.reject(new Error("No outputs in stack description"));
-                    }
+  var stackName = process.env.SERVERLESS_PROJECT + "-" + process.env.SERVERLESS_STAGE + "-r";
+  return CF._describeCFStack(stackName)
+    .then(function(stackDescription) {
+      if (!stackDescription.hasOwnProperty('Outputs') || stackDescription.Outputs.constructor !== Array) {
+        return Promise.reject(new Error("No outputs in stack description"));
+      }
 
-                    return Promise.each(stackDescription.Outputs, function (outVar) {
-                      process.env["SERVERLESS_CF_" + outVar.OutputKey] = outVar.OutputValue;
-                      return null;
-                    });
-                  })
-                  .catch(function (err) {
-                    console.log("WARN: Error retrieving CF variables");
-                  }));
-});
+      return Promise.each(stackDescription.Outputs, function (outVar) {
+        process.env["SERVERLESS_CF_" + outVar.OutputKey] = outVar.OutputValue;
+        return null;
+      });
+    })
+    .catch(function (err) {
+      console.log("WARN: Error retrieving CF variables");
+      return Promise.reject(err);
+    });
+};
 
 /**
  * Get the CF stack description
  */
-CF._describeCFStack = Promise.method(function(stackName) {
-  cloudformation.describeStacks({ StackName: stackname }, function(err, data) {
-    if (err) {
-      throw err;
-    }
-    if (!data.Stacks || data.Stacks.constructor !== Array || data.Stacks.length === 0) {
-      throw new Error("invalid response", data);
-    }
-    // Return only the stack description
-    return data.Stacks[0];
+CF._describeCFStack = function(stackName) {
+  return new Promise(function(resolve,reject) {
+    cloudformation.describeStacks({ StackName: stackName }, function(err, data) {
+      if (err) {
+        reject(err);
+      }
+      
+      if (!data || !data.Stacks || data.Stacks.constructor !== Array || data.Stacks.length === 0) {
+        reject(new Error("invalid response", data));
+      }
+      
+      // Return only the stack description
+      resolve(data.Stacks[0]);
+    });
   })
-});
+};
 
 module.exports = CF;
